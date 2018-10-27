67f6838b00c80b6a5f5f000086848c4d9ffcd66a
pnhung177 <pnhung177@acegik.net>
2018-10-27 10:13:34 +0700

    Moves examples to example_test.go file

--- codetags_test.go
+++ codetags_test.go
@@ -1,66 +1,23 @@
-package codetags_test
+package codetags
 
-import "fmt"
 import "os"
 import "testing"
 import "reflect"
 import "github.com/stretchr/testify/assert"
-import "github.com/saolago/codetags"
 
-func getFirstReturn(manager *codetags.TagManager, err error) *codetags.TagManager {
+func getFirstReturn(manager *TagManager, err error) *TagManager {
   return manager
 }
 
-func ExampleDefault() {
-  tagHandler := codetags.Default()
-
-  // Reload environment variables
-  os.Setenv("CODETAGS_INCLUDED_TAGS", "tag-1,tag-2")
-  os.Setenv("CODETAGS_EXCLUDED_TAGS", "tag-2,tag-3")
-  tagHandler.Reset()
-
-  fmt.Printf("includedTags: %v\n", tagHandler.GetIncludedTags())
-  fmt.Printf("excludedTags: %v\n", tagHandler.GetExcludedTags())
-  // Output:
-  // includedTags: [tag-1 tag-2]
-  // excludedTags: [tag-2 tag-3]
-}
-
 func TestDefault(t *testing.T) {
-  assert.Equal(t, codetags.Default(), getFirstReturn(codetags.GetInstance("CODETAGS")))
-  assert.Equal(t, codetags.Default(), getFirstReturn(codetags.GetInstance("CodeTags")))
-  assert.Equal(t, codetags.Default(), getFirstReturn(codetags.GetInstance("codetags")))
-}
-
-func ExampleNewInstance_01() {
-  os.Setenv("MISSION_INCLUDED_TAGS", "tag-1,tag-2")
-  os.Setenv("MISSION_EXCLUDED_TAGS", "tag-2,tag-3")
-
-  tagHandler, err := codetags.NewInstance("myname", &codetags.Presets{
-    "namespace": "Mission",
-  })
-
-  fmt.Printf("Error: %v\n", err)
-  fmt.Printf("includedTags: %v\n", tagHandler.GetIncludedTags())
-  fmt.Printf("excludedTags: %v\n", tagHandler.GetExcludedTags())
-  // Output:
-  // Error: <nil>
-  // includedTags: [tag-1 tag-2]
-  // excludedTags: [tag-2 tag-3]
-}
-
-func ExampleNewInstance_02() {
-  tagHandler, err := codetags.NewInstance("CODETAGS", &codetags.Presets{
-    "namespace": "Mission",
-  })
-  fmt.Printf("Handler: %v, Error: %v\n", tagHandler, err)
-  // Output:
-  // Handler: <nil>, Error: CODETAGS is default instance name. Please provides another name.
+  assert.Equal(t, Default(), getFirstReturn(GetInstance("CODETAGS")))
+  assert.Equal(t, Default(), getFirstReturn(GetInstance("CodeTags")))
+  assert.Equal(t, Default(), getFirstReturn(GetInstance("codetags")))
 }
 
 func TestNewInstance_invalid_name(t *testing.T) {
   name := ""
-  _, err := codetags.NewInstance(name)
+  _, err := NewInstance(name)
   if err == nil {
     t.Errorf("NewInstance('%s'): must return a non-nil error", name)
   }
@@ -71,7 +28,7 @@ func TestNewInstance_illegal_name(t *testing.T) {
     "codetags", "CodeTags", "CODETAGS",
   }
   for _, name := range table_NewInstance_Names {
-    _, err := codetags.NewInstance(name)
+    _, err := NewInstance(name)
     if err == nil {
       t.Errorf("NewInstance('%s'): must return a non-nil error", name)
     }
@@ -80,18 +37,18 @@ func TestNewInstance_illegal_name(t *testing.T) {
 
 func TestInitialize(t *testing.T) {
   var table_Initialize_Cases = []struct {
-    current *codetags.Presets
-    data *codetags.Presets
-    expected *codetags.Presets
+    current *Presets
+    data *Presets
+    expected *Presets
   }{
     {
-      current: &codetags.Presets { "namespace": "ABC" },
-      data: &codetags.Presets { "namespace": "xyz", "version": "0.1.2" },
-      expected: &codetags.Presets {"namespace": "XYZ", "version": "0.1.2"},
+      current: &Presets { "namespace": "ABC" },
+      data: &Presets { "namespace": "xyz", "version": "0.1.2" },
+      expected: &Presets {"namespace": "XYZ", "version": "0.1.2"},
     },
   }
   for i, c := range table_Initialize_Cases {
-    ct, _ := codetags.NewInstance("test", c.current)
+    ct, _ := NewInstance("test", c.current)
     ct_ref := ct.Initialize(c.data)
     if ct_ref != ct {
       t.Errorf("testcase[%d] - output Ref is different with source Ref", i)
@@ -109,189 +66,22 @@ func TestInitialize(t *testing.T) {
   }
 }
 
-func ExampleTagManager_Register_02() {
-  defer func() {
-    if r := recover(); r != nil {
-      fmt.Println(r)
-    }
-  }()
-
-  tagHandler := codetags.Default().Reset()
-  tagHandler.Register([]interface{} {
-    "feature-1",
-    codetags.TagDescriptor {
-      Name: "feature-2",
-    },
-    1024,
-    codetags.TagDescriptor {
-      Name: "feature-4",
-      Enabled: true,
-    },
-    true,
-  })
-
-  // Output:
-  // descriptor#2 [1024] has invalid type (int), must be a string or TagDescriptor type
-  // descriptor#4 [true] has invalid type (bool), must be a string or TagDescriptor type
-}
-
-// A simple usage: defined a list of tags that can be turned on/off defaultly by Enabled value.
-func ExampleTagManager_Register_01() {
-  tagHandler := codetags.Default().Reset()
-
-  tagHandler.Register([]interface{} {
-    "feature-1",
-    codetags.TagDescriptor {
-      Name: "feature-2",
-    },
-    codetags.TagDescriptor {
-      Name: "feature-3",
-      Enabled: false,
-    },
-    codetags.TagDescriptor {
-      Name: "feature-4",
-      Enabled: true,
-    },
-    codetags.TagDescriptor {
-      Name: "feature-5",
-      Plan: codetags.TagPlan {
-        Enabled: true,
-      },
-    },
-  })
-
-  fmt.Printf("declaredTags: %v", tagHandler.GetDeclaredTags())
-  // Output:
-  // declaredTags: [feature-1 feature-2 feature-4 feature-5]
-}
-
-func ExampleTagManager_Register_03() {
-  tagHandler := codetags.Default().Reset()
-
-  tagHandler.Initialize(&codetags.Presets {
-    "version": "0.1.7",
-  })
-
-  tagHandler.Register([]interface{} {
-    codetags.TagDescriptor {
-      Name: "feature-11",
-      Plan: codetags.TagPlan {
-        Enabled: true,
-      },
-    },
-    codetags.TagDescriptor {
-      Name: "feature-12",
-      Plan: codetags.TagPlan {
-        Enabled: true,
-        MinBound: "0.1.2",
-      },
-    },
-    codetags.TagDescriptor {
-      Name: "feature-13",
-      Plan: codetags.TagPlan {
-        Enabled: true,
-        MinBound: "0.1.2",
-        MaxBound: "0.1.6",
-      },
-    },
-    codetags.TagDescriptor {
-      Name: "feature-14",
-      Plan: codetags.TagPlan {
-        Enabled: false,
-        MinBound: "0.1.2",
-        MaxBound: "0.1.6",
-      },
-    },
-    codetags.TagDescriptor {
-      Name: "feature-15",
-      Plan: codetags.TagPlan {
-        Enabled: true,
-        MinBound: "0.1.8",
-      },
-    },
-    codetags.TagDescriptor {
-      Name: "feature-16",
-      Plan: codetags.TagPlan {
-        Enabled: false,
-        MinBound: "0.1.9",
-      },
-    },
-  })
-
-  fmt.Printf("declaredTags: %v", tagHandler.GetDeclaredTags())
-  // Output:
-  // declaredTags: [feature-11 feature-12 feature-14 feature-16]
-}
-
-func ExampleTagManager_Register_04() {
-  defer func() {
-    if r := recover(); r != nil {
-      fmt.Println(r)
-    }
-  }()
-
-  tagHandler := codetags.Default().Reset()
-
-  tagHandler.Initialize(&codetags.Presets {
-    "version": "0.1.7",
-  })
-
-  tagHandler.Register([]interface{} {
-    "feature-11",
-    codetags.TagDescriptor {
-      Name: "feature-11",
-      Plan: codetags.TagPlan {
-        Enabled: true,
-      },
-    },
-    codetags.TagDescriptor {
-      Name: "feature-12",
-      Plan: codetags.TagPlan {
-        Enabled: true,
-        MinBound: "0.1.2",
-      },
-    },
-    codetags.TagDescriptor {
-      Name: "feature-13",
-      Plan: codetags.TagPlan {
-        Enabled: true,
-        MinBound: "0.1.2",
-        MaxBound: "0.1.6",
-      },
-    },
-    "feature-13",
-    codetags.TagDescriptor {
-      Name: "feature-14",
-      Plan: codetags.TagPlan {
-        Enabled: false,
-        MinBound: "0.1.2",
-        MaxBound: "0.1.6",
-      },
-    },
-    "feature-14",
-  })
-
-  // Output:
-  // Tag [feature-11] is declared more than one time
-  // Tag [feature-14] is declared more than one time
-}
-
 func TestRegister(t *testing.T) {
   var table_Register_Cases = []struct {
-    presets *codetags.Presets
+    presets *Presets
     descriptors []interface{}
     declaredTags []string
   }{
     {
-      presets: &codetags.Presets{ "version": "0.1.2" },
+      presets: &Presets{ "version": "0.1.2" },
       descriptors: []interface{} {
         "tag-1",
-        codetags.TagDescriptor{
+        TagDescriptor{
           Name: "tag-2",
           Enabled: false,
-          Plan: codetags.TagPlan{ Enabled: false },
+          Plan: TagPlan{ Enabled: false },
         },
-        codetags.TagDescriptor{
+        TagDescriptor{
           Name: "tag-3",
         },
       },
@@ -299,7 +89,7 @@ func TestRegister(t *testing.T) {
     },
   }
   for i, c := range table_Register_Cases {
-    ct, _ := codetags.NewInstance("test", c.presets)
+    ct, _ := NewInstance("test", c.presets)
     ct_ref := ct.Register(c.descriptors)
     if ct_ref != ct {
       t.Errorf("testcase[%d] - output Ref is different with source Ref", i)
@@ -315,7 +105,7 @@ func TestIsActive(t *testing.T) {
   os.Setenv("ISACTIVE_INCLUDED_TAGS", "abc, def, xyz, tag-4")
   os.Setenv("ISACTIVE_EXCLUDED_TAGS", "disabled, tag-2")
 
-  isacti, _ := codetags.NewInstance("isacti", &codetags.Presets{
+  isacti, _ := NewInstance("isacti", &Presets{
     "namespace": "IsActive",
   })
 
